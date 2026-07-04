7c1937fe760c951eefd426ae379348e5572833b6
Arpit-Uttam <188016518+Arpit-Uttam@users.noreply.github.com>
2026-07-04 14:08:31 +0530

    Create laptopclass.java

---
+++ Hibernate/Mapping/onetoone/laptopclass.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Laptop {
+    @Id
+    private int lid;
+    private String brand;
+    private int ram;
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "lid=" + lid +
+                ", brand='" + brand + '\'' +
+                ", ram=" + ram +
+                '}';
+    }
+}
