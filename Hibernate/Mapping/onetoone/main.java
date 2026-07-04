2ea29a34cebc36c6ad8bd22537071f1758555ad7
Arpit-Uttam <188016518+Arpit-Uttam@users.noreply.github.com>
2026-07-04 14:07:42 +0530

    Implement Hibernate one-to-one mapping example

---
+++ Hibernate/Mapping/onetoone/main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        Laptop l1=new Laptop();
+        l1.setLid(1);
+        l1.setBrand("acer");
+        l1.setRam(34);
+
+        Alian a1=new Alian();
+        a1.setAid(1);
+        a1.setAname("iri");
+        a1.setTech("java");
+        a1.setLaptop(l1);
+
+//        Configuration cfg=new Configuration();
+//        cfg.addAnnotatedClass(org.example.Student.class);
+//        cfg.configure();
+        SessionFactory sf= new Configuration().addAnnotatedClass(org.example.Alian.class).addAnnotatedClass(org.example.Laptop.class).configure().buildSessionFactory();      //cfg.buildSessionFactory();  refactoring
+        Session session= sf.openSession();
+        Transaction transaction= session.beginTransaction();
+
+        session.persist(a1);
+        session.persist(l1);
+        transaction.commit();
+        session.close();
+        sf.close();
+        System.out.println(a1);
+
+    }
+}
