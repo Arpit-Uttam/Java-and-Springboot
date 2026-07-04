f55ff7e7b2116bbe193930e16872b3890bdf26e5
Arpit-Uttam <188016518+Arpit-Uttam@users.noreply.github.com>
2026-07-04 10:32:07 +0530

    Add Laptop class as an embeddable entity

---
+++ Hibernate/Embeddable/laptopclass.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Laptop {
+    private String brand;
+    private int ram;
+    private String color;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "brand='" + brand + '\'' +
+                ", ram=" + ram +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
